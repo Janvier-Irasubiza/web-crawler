e860fcc2f77ff746e46a922bc11f127f19920027
Janvier-Irasubiza <61458240+Janvier-Irasubiza@users.noreply.github.com>
2025-05-18 14:52:49 +0200

    fix: Update server host to allow external access

--- app.py
+++ app.py
@@ -382,4 +382,4 @@ if __name__ == "__main__":
     start_crawler()
     start_analytics_server()
     # Start the server
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
